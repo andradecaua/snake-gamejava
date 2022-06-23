8dca6d2da8c0707482ba6098ee5b55777c19936e
Cauã Andrade <79015834+Carinha-csa@users.noreply.github.com>
2022-06-23 09:44:52 -0300

    Add files via upload

---
+++ src/snakegame/Snake.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package snakegame;
+
+/**
+ *
+ * @author Cauã
+ */
+public class Snake {
+    
+    public static void main(String args[]){
+        
+        new GameFrame();
+      
+    }
+}
